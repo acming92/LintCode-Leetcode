2f40a9bac0c6de4ad854428b38819c492cfc57e9
Qiming Chen <qimingchen@Qimings-MacBook-Pro.local>
2015-08-28 16:21:06 -0700

    Binary Tree Level order

---
+++ BinaryTreeLevelOrderTraversal/src/Solution.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Solution {
+		public static class TreeNode {
+	      public int val;
+	      public TreeNode left, right;
+	      public TreeNode(int val) {
+	          this.val = val;
+	          this.left = this.right = null;
+	      }
+	  }
+		
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        // write your code here
+    	ArrayList<TreeNode> list = new ArrayList<TreeNode>();
+    	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+
+    	if(root == null) {
+    		return result;
+    	}
+    	list.add(root);
+    	
+    	while(!list.isEmpty()) {
+    		int length = list.size();
+    		ArrayList<Integer> level = new ArrayList<Integer>();
+    		while(--length >= 0) {
+    			TreeNode node = list.remove(0);
+    			level.add(node.val);
+    			if(node.left != null) {
+        			list.add(node.left);
+    			}
+    			if(node.right != null) {
+    				list.add(node.right);
+    			}
+    		}
+    		result.add(level);
+    	}
+    	
+    	return result;
+    }
+    
+
+}
