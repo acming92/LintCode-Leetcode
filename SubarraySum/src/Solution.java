a893c1d2b1a85c6b32e5bb8d3e84cc624f28ab45
Qiming Chen <qimingchen@Qimings-MacBook-Pro.local>
2015-09-11 14:52:21 -0700

    sub array sum

---
+++ SubarraySum/src/Solution.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number 
+     *          and the index of the last number
+     */
+    public ArrayList<Integer> subarraySum(int[] nums) {
+        // write your code here
+    	HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    	ArrayList<Integer> result = new ArrayList<Integer>(); 
+    	
+    	map.put(0, -1);
+    	int sum = 0;
+    	for(int i = 0; i < nums.length; ++i) {
+    		sum += nums[i];
+    		if(map.containsKey(sum)) {
+    			result.add(map.get(sum) + 1);
+    			result.add(i);
+    			return result;
+    		}
+    		
+    		map.put(sum, i);
+    	}
+    	
+    	return result;
+    }
+    
+    public static void main(String[] args) {
+    	
+    }
+}
